dd6971cf1b0839d6463cb1003d39c54b0fe7234c
Ben Hood <0x6e6562@gmail.com>
2014-01-30 16:36:20 +0000

    Version uprev

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ import (
 var opts generator.Options
 var parser = flags.NewParser(&opts, flags.Default)
 
-var VERSION string = "0.9"
+var VERSION string = "0.9.1"
 
 func init() {
 	opts.Version = printVersionAndExit
