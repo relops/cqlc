163411646bddca429d40b88d706b449a93f1faf3
Ben Hood <0x6e6562@gmail.com>
2014-03-25 13:01:45 +0000

    Version up rev to match new runtime

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 var opts generator.Options
 var parser = flags.NewParser(&opts, flags.Default)
 
-var VERSION string = "0.9.26"
+var VERSION string = "0.9.27"
 
 func init() {
 	opts.Version = printVersionAndExit
